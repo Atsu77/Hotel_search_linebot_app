8f31ce9ee98229aad53017ce448d5d5150269890
Atsu <kamnknon@gmail.com>
2021-04-18 16:09:02 +0900

    署名の検証

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,6 +2,13 @@ class LineBotController < ApplicationController
     protect_from_forgery except:[:callback]
 
     def callback
+        body = request.body.read
+        signature = request.env['HTTP_X_LINE_SIGNATURE']
+        unless client.validate_signature(body, signature)
+            p '不正なリクエストです。'
+            return head :bad_request
+        end
+        p '正しいリクエストです。'
     end
 
     private
